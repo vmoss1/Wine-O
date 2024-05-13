043f30ec2e1765963b0e13bf2eca4a8529ceeca4
Veronica Moss <vivettemoss@yahoo.com>
2024-05-13 15:52:39 -0500

    added validation middleware signin/signup

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,11 +3,24 @@ const { Op } = require("sequelize");
 const bcrypt = require("bcryptjs");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
 
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
 // Log in
-router.post("/", async (req, res, next) => {
+router.post("/", validateLogin, async (req, res, next) => {
   const { credential, password } = req.body;
 
   const user = await User.unscoped().findOne({
@@ -39,7 +52,6 @@ router.post("/", async (req, res, next) => {
     user: safeUser,
   });
 });
-
 // Restore session user
 router.get("/", (req, res) => {
   const { user } = req;
