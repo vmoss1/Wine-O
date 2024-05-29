8a61cd3b8b4af58783446a54bbcfb084a6caf55f
Veronica Moss <vivettemoss@yahoo.com>
2024-05-29 10:51:39 -0500

    fixed vite.config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
+
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
@@ -6,9 +11,15 @@ export default defineConfig(({ mode }) => ({
       failOnError: mode === "production",
     }),
   ],
+  // proxy will force the frontend server to act like it's being served from the backend server
   server: {
     proxy: {
       "/api": "http://localhost:8000",
     },
   },
+  // To automatically open the app in the browser whenever the server starts,
+  // uncomment the following lines:
+  // server: {
+  //   open: true
+  // }
 }));
