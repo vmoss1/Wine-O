025156a916b5f7fd0aa5d7eb937332e634db6c93
Veronica Moss <vivettemoss@yahoo.com>
2024-06-26 18:30:58 -0500

    removed wine store

--- frontend/src/store/wine.js
+++
@@ -1,9 +0,0 @@
-import { csrfFetch } from "./csrf";
-
-const GET_ALL_WINES = 'wines/getWines'
-const GET_SINGLE_WINE = 'wines/getSingleWine'
-
-const getWines = (wines) => ({
-    type: GET_ALL_WINES,
-    payload: wines
-})
