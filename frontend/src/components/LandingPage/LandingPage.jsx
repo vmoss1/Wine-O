28cbfbc95c65878388d8be4944efdbe98a33c19b
Veronica Moss <vivettemoss@yahoo.com>
2024-07-11 13:19:41 -0500

    editing landing page

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -10,12 +10,9 @@ function LandingPage() {
       <div id="landing-page-about">
         <div id="landing-page-paragraph">
           <h3 id="landing-page-h3">
-            Wine-O is a vibrant, neon-colored wine app designed for friends who
-            love to share and rate their favorite wines. With a retro pixel
-            theme, VinoVibe offers a fun and engaging way to explore new wines,
-            keep track of your tasting experiences, and see what your friends
-            are enjoying. Discover, rate, and discuss wines together, making
-            every bottle a shared adventure!
+            Wine-O with a retro pixel theme, offers a fun and engaging way to
+            explore new wines, keep track of your tasting experiences, and see
+            what your friends are enjoying!
           </h3>
         </div>
         <div id="landing-page-image">
