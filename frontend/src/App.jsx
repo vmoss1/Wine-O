e6139e661bc2e088cbbf44909bc7a059391ae5d2
Veronica Moss <vivettemoss@yahoo.com>
2024-07-15 13:33:40 -0500

    added landing page modal design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,6 +33,10 @@ const router = createBrowserRouter([
         path: "/",
         element: <LandingPage />,
       },
+      {
+        path: "/wines",
+        element: <LandingPage />,
+      },
     ],
   },
 ]);
